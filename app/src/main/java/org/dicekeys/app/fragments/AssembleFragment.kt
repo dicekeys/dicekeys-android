5ed112a158a5d9d5675a76b1655eaa0ba2d5e1ae
Angelos Veglektsis <angelos@aveworks.com>
2021-02-25 10:46:58 +0200

    Fix #74 (#81)

--- app/src/main/java/org/dicekeys/app/fragments/AssembleFragment.kt
+++ app/src/main/java/org/dicekeys/app/fragments/AssembleFragment.kt
@@ -156,6 +156,8 @@ class AssembleFragment : AppFragment<AssembleFragmentBinding>(R.layout.assemble_
             }
 
             view.findViewById<Button>(R.id.btn_scan)?.setOnClickListener {
+                // Clear previous dicekey
+                viewModel.setDiceKey(null)
                 parent.navigate(AssembleFragmentDirections.actionGlobalScanFragment())
             }
 
--- app/src/main/java/org/dicekeys/app/viewmodels/AssembleViewModel.kt
+++ app/src/main/java/org/dicekeys/app/viewmodels/AssembleViewModel.kt
@@ -3,6 +3,7 @@ package org.dicekeys.app.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.dicekeys.app.adapters.dicekey
 import org.dicekeys.app.repositories.DiceKeyRepository
 import org.dicekeys.dicekey.DiceKey
 import org.dicekeys.dicekey.Face
@@ -13,14 +14,19 @@ import javax.inject.Inject
 class AssembleViewModel @Inject constructor(val diceKeyRepository: DiceKeyRepository) : ViewModel() {
     var page = MutableLiveData(0)
 
-    var diceKey = MutableLiveData<DiceKey<Face>>()
+    var diceKey = MutableLiveData<DiceKey<Face>?>()
 
     // var diceKeyScanned = MutableLiveData(false)
     var diceKeyBackedUp = MutableLiveData(false)
 
-    fun setDiceKey(dk: DiceKey<Face>){
+    fun setDiceKey(dk: DiceKey<Face>?){
+        // remove the previous dicekey from memory
+        diceKey.value?.let {
+            diceKeyRepository.remove(it)
+        }
+
         diceKey.value = dk
-        diceKeyRepository.set(dk)
+        dk?.let { diceKeyRepository.set(it) }
     }
 
     fun setPage(position: Int) {
