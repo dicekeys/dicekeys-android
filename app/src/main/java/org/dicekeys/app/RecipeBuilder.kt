65864cbd9981427c816f2f28606e270abd5c6349
Angelos Veglektsis <angelos@aveworks.com>
2021-10-15 17:27:09 +0300

    RecipeBuilder: Remove subdomain if entry is URL

--- app/src/main/java/org/dicekeys/app/RecipeBuilder.kt
+++ app/src/main/java/org/dicekeys/app/RecipeBuilder.kt
@@ -7,8 +7,8 @@ import java.net.URL
 /*
  * A builder to create custom or template based Recipes
  */
-class RecipeBuilder constructor(val type : DerivationOptions.Type, val template: DerivationRecipe?) {
-     var domains: String? =null
+class RecipeBuilder constructor(val type: DerivationOptions.Type, val template: DerivationRecipe?) {
+    var domains: String? = null
         private set
     var purpose: String? = null
         private set
@@ -16,7 +16,7 @@ class RecipeBuilder constructor(val type : DerivationOptions.Type, val template:
     private var lengthInChars = 0
     private var lengthInBytes = 0
 
-    fun reset(){
+    fun reset() {
         domains = null
         purpose = null
         sequence = 1
@@ -24,33 +24,40 @@ class RecipeBuilder constructor(val type : DerivationOptions.Type, val template:
         lengthInBytes = 0
     }
 
-    fun updateDomains(d: String){
+    fun updateDomains(d: String) {
         domains = d
         purpose = null
     }
 
-    fun updatePurpose(p : String?){
+    fun updatePurpose(p: String?) {
         purpose = p
     }
 
-    fun updateSequence(s: Int){
-        sequence = if(s >= 1) s else 1
+    fun updateSequence(s: Int) {
+        sequence = if (s >= 1) s else 1
     }
 
     fun updateLengthInChars(length: Int) {
-        lengthInChars = if(length in 16..999) length else 0
+        lengthInChars = if (length in 16..999) length else 0
     }
 
     fun updateLengthInBytes(length: Int) {
-        lengthInBytes = if(length in 16..999) length else 0
+        lengthInBytes = if (length in 16..999) length else 0
     }
 
     private fun getDomainList(): List<String> {
         // Check if is valid URL
-        try{
+        // On URLs remove the subdomain eg. keep last 2 elements of host, subdomain1.subdomain2.google.com -> google.com
+        try {
             val url = URL(domains)
-            return listOf(url.host)
-        }catch (e: Exception){
+            return listOf(
+                url.host
+                    .split(".").let {
+                        it.filterIndexed { i, _ -> i >= (it.size - 2) }
+                            .joinToString(".")
+                    }
+            )
+        } catch (e: Exception) {
             e.printStackTrace()
         }
 
@@ -63,9 +70,9 @@ class RecipeBuilder constructor(val type : DerivationOptions.Type, val template:
     }
 
     fun getDerivationRecipe(): DerivationRecipe? {
-        return if(template == null) {
+        return if (template == null) {
 
-            if(purpose.isNullOrBlank()) {
+            if (purpose.isNullOrBlank()) {
                 DerivationRecipe.createCustomOnlineRecipe(
                     type = type,
                     domains = getDomainList(),
@@ -73,10 +80,10 @@ class RecipeBuilder constructor(val type : DerivationOptions.Type, val template:
                     lengthInChars = lengthInChars,
                     lengthInBytes = lengthInBytes
                 )
-            }else{
+            } else {
                 DerivationRecipe.createCustomPurposeRecipe(
                     type = type,
-                    purpose =purpose ?: "",
+                    purpose = purpose ?: "",
                     sequenceNumber = sequence,
                     lengthInChars = lengthInChars,
                     lengthInBytes = lengthInBytes
--- app/src/test/java/org/dicekeys/app/RecipeBuilderUnitTests.kt
+++ app/src/test/java/org/dicekeys/app/RecipeBuilderUnitTests.kt
@@ -17,6 +17,7 @@ class RecipeBuilderUnitTests {
 
     companion object{
         val GOOGLE = DerivationRecipe(DerivationOptions.Type.Password, "google.com", """{"allow":[{"host":"*.google.com"}],"#":12}""")
+        val GOOGLE_SUBDOMAIN = DerivationRecipe(DerivationOptions.Type.Password, "subdomain.google.com", """{"allow":[{"host":"*.subdomain.google.com"}],"#":12}""")
         val GOOGLE_FOOD = DerivationRecipe(DerivationOptions.Type.Password, "food.com, google.com", """{"allow":[{"host":"*.food.com"},{"host":"*.google.com"}]}""")
         val APPLE = DerivationRecipe(DerivationOptions.Type.Password, "apple.com, icloud.com", """{"allow":[{"host":"*.apple.com"},{"host":"*.icloud.com"}],"lengthInChars":64,"#":2}""")
     }
@@ -39,9 +40,13 @@ class RecipeBuilderUnitTests {
         Assert.assertEquals(GOOGLE, builder.getDerivationRecipe())
 
         builder.reset()
+        builder.updateDomains(".subdomain.google.com/")
+        builder.updateSequence(12)
+        Assert.assertEquals(GOOGLE_SUBDOMAIN, builder.getDerivationRecipe())
+
+        builder.reset()
         builder.updateDomains("google.com,food.com")
         Assert.assertEquals(GOOGLE_FOOD, builder.getDerivationRecipe())
-
 
         builder.reset()
         builder.updateDomains(".apple.com/,.icloud.com/")
@@ -53,12 +58,17 @@ class RecipeBuilderUnitTests {
     @Test
     fun test_predefinedSolutions_Url(){
 
-        builder.updateDomains("https://google.com/?q=DiceKeys")
-        builder.updateSequence(12)
-        Assert.assertEquals(GOOGLE, builder.getDerivationRecipe())
+//        builder.updateDomains("https://google.com/?q=DiceKeys")
+//        builder.updateSequence(12)
+//        Assert.assertEquals(GOOGLE, builder.getDerivationRecipe())
+//
+//        builder.reset()
+//        builder.updateDomains("http://google.com/search?q=DiceKeys")
+//        builder.updateSequence(12)
+//        Assert.assertEquals(GOOGLE, builder.getDerivationRecipe())
 
         builder.reset()
-        builder.updateDomains("http://google.com/search?q=DiceKeys")
+        builder.updateDomains("http://subdomain.google.com/search?q=DiceKeys")
         builder.updateSequence(12)
         Assert.assertEquals(GOOGLE, builder.getDerivationRecipe())
     }
